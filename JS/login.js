6a0aab24d836ae859694715c2dbecfea2f8ca369
Tanisha sain <131015973+Tanisha-sain@users.noreply.github.com>
2024-11-07 15:47:08 +0530

    Create login.js

---
+++ JS/login.js
@@ -0,0 +1,13 @@
+const user = document.getElementById('username').value;
+const pass = document.getElementById('pass').value;
+
+let valid = false;
+if(user == "admin" && pass == "admin"){
+    valid = true;
+}
+
+if(valid){
+
+}else{
+    
+}
