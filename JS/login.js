e3566d5fb815aee4ba80bf7c5841d38bc2730447
Tanisha sain <131015973+Tanisha-sain@users.noreply.github.com>
2024-11-09 14:20:25 +0530

    Updated

--- JS/login.js
+++ JS/login.js
@@ -1,13 +1,19 @@
-const user = document.getElementById('username').value;
-const pass = document.getElementById('pass').value;
+const user = document.getElementById('username');
+const pass = document.getElementById('pass');
+const btn = document.getElementById('login');
 
-let valid = false;
-if(user == "admin" && pass == "admin"){
-    valid = true;
-}
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    if(user.value == "admin" && pass.value == "admin"){
+        user.value = ""
+        pass.value= ""
+        window.location.href = "welcome.html"
+    }else{
+        user.value = "";
+        pass.value = "";
+        alert("Please enter valid credentials !!");
+    }
+})
 
-if(valid){
 
-}else{
-    
-}
+
